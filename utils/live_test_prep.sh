3242c86e7b1e1db458c13959facd0d6527bc0b29
Govinda Keshavdas <gkeshavdas@google.com>
2024-01-26 01:50:35 +0000

    Remove accidental additions to utils/

--- utils/live_test_prep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install virtualenv python3-venv
-python3 -m venv /tmp/myproject
-source /tmp/myproject/bin/activate
-pip install timeout-decorator 
-pip install -r requirements.txt
-source /tmp/myproject/bin/activate
